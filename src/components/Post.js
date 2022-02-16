85c05ec276d95c2b763467f70a6ef12c63992ace
Barbu <dayle.parent@outlook.fr>
2022-02-16 16:02:09 +0100

    {feat} réception des posts depuis BDD

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,31 @@
+import { GrLike } from "react-icons/gr";
+import { useState } from "react";
+
+// import { Button } from "antd";
+
 function Post(props) {
     const postContent = props.content;
+
+    const [likes, setLikes] = useState(postContent.likes);
+
+    const handleLikes = (event) => {
+        console.log(event);
+        setLikes(likes + 1);
+    };
+
     return (
         <div className="Post">
             <h2>{postContent.nom}</h2>
             <p>{postContent.contenu}</p>
+            <div>
+                <div>
+                    <h3>{likes}</h3>
+                    <GrLike onClick={handleLikes}></GrLike>
+                </div>
+                <div>
+                    <h3>{postContent.comments}</h3>
+                </div>
+            </div>
         </div>
     );
 }
