7b86a76a613234ce2e5de35bfeb0d4dc119030ff
Barbu <dayle.parent@outlook.fr>
2022-02-17 13:43:39 +0100

    Correction fichier

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import CreateAccount from "./createAccount";
 import { Link } from "react-router-dom";
 import "../styles/Login.css";
 /**
