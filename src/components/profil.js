aefedf095b7460f023f62b4c228a424c45b78e28
Barbu <dayle.parent@outlook.fr>
2022-02-22 08:56:35 +0100

    Ajout infos compte + correction code

--- src/components/profil.js
+++ src/components/profil.js
@@ -2,7 +2,9 @@ import { useState } from "react";
 import React from "react";
 import { useNavigate } from "react-router";
 import Header from "../assets/mountain-lake-header_1.jpg";
+import Urbex from "../assets/urbexpic.jpg";
 import Logo from "../assets/Logo.png";
+import { useEffect } from "react";
 import "../styles/profil.css";
 
 function Profil(props) {
@@ -11,29 +13,23 @@ function Profil(props) {
         setDisplayForm(!displayForm);
     };
 
-    let [nom, setNom] = useState("");
-    const handleChangeNomField = (event) => {
-        setNom(event.target.value);
-    };
+    let [userData, setUserData] = useState({});
+    useEffect(async () => {
+        const response = await fetch("http://localhost:3002/profil", {
+            method: "GET",
+            headers: {
+                Authorization: localStorage.getItem("@token"),
+                "Content-Type": "application/json",
+            },
+        });
 
-    let [prenom, setPrenom] = useState("");
-    const handleChangePrenomField = (event) => {
-        setPrenom(event.target.value);
-    };
-
-    let [naissance, setNaissance] = useState("");
-    const handleChangeNaissanceField = (event) => {
-        setNaissance(event.target.value);
-    };
-
-    let [pseudo, setPseudo] = useState("");
-    const handleChangePseudoField = (event) => {
-        setPseudo(event.target.value);
-    };
+        let userInfo = await response.json();
+        setUserData(userInfo);
+    }, []);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(nom);
+        console.log();
         return false;
     };
 
@@ -61,12 +57,52 @@ function Profil(props) {
                 </div>
             </div>
             <div className="MyPost">
-                <div className="headerPost">
-                    <h2>My previous post</h2>
-                </div>
-                <div className="myPreviousPost">
-                    <h2>Titre</h2>
-                    <p>Le contenu</p>
+                <div className="headerPost"></div>
+                <div className="mainBody">
+                    <div className="leftBody">
+                        <div className="infoUser">
+                            <h3>About me</h3>
+                            <ul className="listAbout">
+                                <li>Nom : {userData.nom}</li>
+                                <li>Prenom : {userData.prenom}</li>
+                                <li>Date de Naissance : {userData.naissance}</li>
+                                <li>Pseudo : {userData.pseudo}</li>
+                            </ul>
+                        </div>
+                        <div className="Amis">
+                            <h3>Mes Amis</h3>
+                            <ul className="listAbout">
+                                <li>Jean</li>
+                                <li>Luc</li>
+                                <li>Josiane</li>
+                                <li>Pepito</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="myPreviousPost">
+                        <div className="previousPost">
+                            <h3>My Previous Post</h3>
+                        </div>
+                        <h2>A Green Paradise Lost</h2>
+                        <img src={Urbex} className="urbex" />
+                        <h4>L'exploration du jardin de Poison Ivy</h4>
+                        <p>
+                            C'est avec grande surprise que nous sommes tombés sur cette serre abandonnée derrière une
+                            maison de ville. La maison était délabrée et depuis quelques années déjà les squatteurs
+                            avaient établi leur campement. C'était évidemment une occasion formidable pour nous de
+                            s'embarquer dans de l'Urbex comme on l'aime ! Si cette exploration vous intéresse (et je
+                            vous la recommande), lisez ci dessous les instructions pour une exploration en confort et en
+                            toute sécurité :
+                        </p>
+                        <p>Equipements nécessaire :</p>
+                        <ul className="listeEquipement">
+                            <li>- Veste chaude (atmosphère très humide)</li>
+                            <li>- Lampe Torche</li>
+                            <li>- Appareil Photo / Camescope</li>
+                            <li>- Couteau</li>
+                            <li>- Pince (certain grillage était difficile à escalader</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
