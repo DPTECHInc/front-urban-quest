069f55e4f0726b883b0ad7923f8eb0898cba5d61
Barbu <dayle.parent@outlook.fr>
2022-02-22 08:55:51 +0100

    Redirection lors login + modif code

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../styles/Login.css";
 /**
  *  Source from https://contactmentor.com/login-form-react-js-code/
@@ -10,7 +10,7 @@ function Login(props) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [backendResponse, setBackendResponse] = useState("");
-
+    const navigate = useNavigate();
     function validateForm() {
         return email.length > 0 && password.length > 0;
     }
@@ -49,8 +49,8 @@ function Login(props) {
         if (responseBody.token) {
             // Stockage
             localStorage.setItem("@token", responseBody.token);
-            console.log("you are now logged in");
-            console.log("//TODO : clear form, and return back to home");
+            console.log("Vous êtes connecté");
+            navigate("/");
             return;
         }
     }
@@ -60,11 +60,22 @@ function Login(props) {
             <Form onSubmit={handleSubmit} method="POST">
                 <Form.Group size="lg" controlId="email">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control autoFocus type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <Form.Control
+                        autoFocus
+                        type="email"
+                        autocomplete="on"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </Form.Group>
                 <Form.Group size="lg" controlId="password">
                     <Form.Label>Mot de passe</Form.Label>
-                    <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <Form.Control
+                        type="password"
+                        autocomplete="on"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </Form.Group>
                 <Button size="lg" type="submit" disabled={!validateForm()}>
                     Se connecter
